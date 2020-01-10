534376e11c4ff27c4eabdae4c2ff61aa3501c783
sAnjali12 <anjalisen060@gmail.com>
2020-01-10 10:11:37 +0530

    I changed some variable names

--- Routes/view/script.js
+++ Routes/view/script.js
@@ -1,11 +1,11 @@
-const questions = [];
+// const questions = [];
 
-$(function fetchdata() {
-    $.get('http://localhost:3000/get', {},
-    (data,resp) => {
-        console.log(data)
-        data.map((data), (question)=> {
-            $('questions').show()
-        })
-    });
-})
+// $(function fetchdata() {
+//     $.get('http://localhost:3000/get', {},
+//     (data,resp) => {
+//         console.log(data)
+//         data.map((data), (question)=> {
+//             $('questions').show()
+//         })
+//     });
+// })
